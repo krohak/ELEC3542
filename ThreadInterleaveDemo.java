028639689e597a05dc8855dd26ebf54cf9caa710
Rohak <rohak@localhost.localdomain>
2017-01-21 20:14:00 +0800

    first

---
+++ ThreadInterleaveDemo.java
@@ -0,0 +1,54 @@
+public class ThreadInterleaveDemo {
+
+	static class PrintOddThread extends Thread {
+	
+		// Print out odd numbers from 1 to 10
+		public void run() {
+			for (int i = 1; i <= 10; i = i + 2) {
+				System.out.print(i + " ");
+
+				// Idle for 10 ms after each print
+				try {
+					Thread.currentThread().sleep(10);
+				} catch (InterruptedException ie) {
+					System.exit(1);
+				}
+			}
+		}
+
+	}
+
+	static class PrintEvenThread extends Thread {
+
+		// Print out even numbers from 1 to 10
+		public void run() {
+			for (int i = 2; i <= 10; i = i + 2) {
+				System.out.print(i + " ");
+
+				// Idle for 10 ms after each print
+				try {
+					Thread.currentThread().sleep(10);
+				} catch (InterruptedException ie) {
+					System.exit(1);
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		PrintOddThread thread1 = new PrintOddThread();
+		PrintEvenThread thread2 = new PrintEvenThread();
+		thread1.start();
+		thread2.start();
+
+		try {
+			Thread.currentThread().sleep(100);
+		} catch (InterruptedException ie) {
+			System.exit(1);
+		}
+
+		System.out.println();
+	}
+
+}
