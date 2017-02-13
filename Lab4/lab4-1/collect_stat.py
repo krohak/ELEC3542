bb6839065c896fe64784076d36382d773332ad74
krohak <rohaksinghal14@gmail.com>
2017-02-13 04:39:58 +0000

    quicksort rpi

--- Lab4/lab4-1/collect_stat.py
+++ Lab4/lab4-1/collect_stat.py
@@ -1,11 +1,11 @@
 import subprocess
 
-f = open('data.txt', 'w')
+f = open('data_quicksort.txt', 'w')
 sizes = []
 for i in range(5, 20):
 	sizes.append(i * 1000000)
 
-command = '/usr/bin/java TimeSpaceMergeSort '
+command = '/usr/bin/java TimeQuickSort '
 for i in sizes:
-	p = command + str(i) + " >> data.txt"
+	p = command + str(i) + " >> data_quicksort.txt"
 	subprocess.call(p, shell=True)
