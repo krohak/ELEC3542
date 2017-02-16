0d4a9b6abf51273458c73af6d095db4d27fcaa10
krohak <rohaksinghal14@gmail.com>
2017-02-16 12:14:46 +0800

    4.2

--- Lab4/lab4-2/ArrayMatrix.java
+++ Lab4/lab4-2/ArrayMatrix.java
@@ -8,21 +8,82 @@ public class ArrayMatrix extends MyMatrix {
     }
 
     public boolean addElt(int row, int col, int element) {
+
+        elt[row][col] = element;
+
         return true;
     }
 
     public void printMatrix() {
+
+      System.out.println("Row: " + row + "; Col: " + col);
+
+      for (int i=0; i<row; i++){
+
+          for (int j=0; j<col; j++){
+
+          if(elt[i][j] != 0){
+
+              System.out.print("[" + i + ", " + j + "," + elt[i][j] + "] ");
+
+          }
+
+
+
+        }
+
+
+
+      }
+      System.out.println();
 
     }
 
     public int findLargestElt() {
         int max = 0;
 
+
+        for (int i=0; i<row; i++){
+
+          for (int j=0; j<col; j++){
+
+            if(elt[i][j]>max){
+
+                max = elt[i][j];
+
+            }
+
+
+
+          }
+
+
+
+        }
+
+
+System.out.println();
+
         return max;
     }
 
     public int sumColumn(int col) {
         int sum = 0;
+
+
+        for (int i=0; i<row; i++){
+
+
+            if(elt[i][col] != 0){
+
+                sum = sum + elt[i][col];
+
+            }
+
+        }
+
+
+//System.out.println();
 
         return sum;
     }
--- Lab4/lab4-2/SparseMatrixDriver.java
+++ Lab4/lab4-2/SparseMatrixDriver.java
@@ -15,30 +15,60 @@ public class SparseMatrixDriver {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         int numRow, numCol;
 
-        numRow = numCol = 100;
-        ListMatrix m = new ListMatrix(numRow, numCol);
-        // ArrayMatrix m = new ArrayMatrix(numRow, numCol);
+        long startTime, endTime, timeNeeded;
 
+        if (args.length < 1) {
+            System.out.println("You have to input the size of array");
+            System.exit(1);
+        }
+
+        int size = 0;
+        int batch = 0;
+        try {
+            size = Integer.parseInt(args[0]);
+            batch = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("You have to input valid numbers.");
+            System.exit(1);
+        }
+
+
+        numRow = numCol = size;
+        //ListMatrix m = new ListMatrix(numRow, numCol);
+         ArrayMatrix m = new ArrayMatrix(numRow, numCol);
+         System.out.println("The memory used after creating the input array is: " + MeasureMemory.memoryUsed());
+         /*
         m.addElt(2, 3, 4);
         m.addElt(1, 1, 5);
         m.addElt(2, 1, 7);
         m.addElt(3, 3, 2);
         m.addElt(2, 2, 3);
+        */
+         batchAddElt(batch, m);
 
-        // batchAddElt(10, m);
+
+        startTime = System.currentTimeMillis();
 
         m.printMatrix();
 
-        /*
+        endTime = System.currentTimeMillis();
+        timeNeeded = endTime - startTime;
+        System.out.print("The time needed to print " + numRow +"x" + numCol + " Array is " + timeNeeded + " ms.");
+
+      //  /*
         int largestElt = m.findLargestElt();
         System.out.println("The largest element in the matrix is: " + largestElt);
 
+        startTime = System.currentTimeMillis();
         int sumOfCol = m.sumColumn(1);
+        endTime = System.currentTimeMillis();
+        timeNeeded = endTime - startTime;
         System.out.println("The sum of column 1 is: " + sumOfCol);
-        */
+        System.out.println("The time needed to print the sum of column 1 is " + timeNeeded + " ms.");
+    //  */
     }
 
 }
