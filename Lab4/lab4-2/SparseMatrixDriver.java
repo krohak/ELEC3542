4ec47f4f6935513b8f99a8430f27c93b26dc9ed3
krohak <rohaksinghal14@gmail.com>
2017-02-18 03:25:21 +0800

    pc data

--- Lab4/lab4-2/SparseMatrixDriver.java
+++ Lab4/lab4-2/SparseMatrixDriver.java
@@ -37,8 +37,8 @@ public class SparseMatrixDriver {
 
 
         numRow = numCol = size;
-        //ListMatrix m = new ListMatrix(numRow, numCol);
-        ArrayMatrix m = new ArrayMatrix(numRow, numCol);
+        ListMatrix m = new ListMatrix(numRow, numCol);
+        //ArrayMatrix m = new ArrayMatrix(numRow, numCol);
         batch = (int) ((batch*size*size)/100);
          /*
         m.addElt(2, 3, 4);
--- Lab4/lab4-2/collect_stat.py
+++ Lab4/lab4-2/collect_stat.py
@@ -1,11 +1,11 @@
 import subprocess
 
-f = open('percent_matrixdriver_2D.txt', 'w')
+#f = open('percent_matrixdriver_linked.txt', 'w')
 sizes = [100,500,1000,2000,5000]
 
 command = '/usr/bin/java SparseMatrixDriver '
 for i in sizes:
 	for j in range(5,100,5):
-		p = command + str(i) + " " + str(j)+ " >> " + str(i) + "_percent_matrixdriver_2D.txt"
+		p = command + str(i) + " " + str(j)+ " >> " + str(i) + "_percent_matrixdriver_linked.txt"
 		subprocess.call(p, shell=True)
 		print(p)
