796740b64b210ee618ccc3c3b4084a5d4411fa02
krohak <rohaksinghal14@gmail.com>
2017-02-18 01:10:43 +0000

    rpi data

--- Lab4/lab4-2/collect_stat.py
+++ Lab4/lab4-2/collect_stat.py
@@ -1,11 +1,12 @@
 import subprocess
 
 #f = open('percent_matrixdriver_linked.txt', 'w')
-sizes = [100,500,1000,2000,5000]
+#sizes = [100,500,1000,2000,5000]
+sizes = [5000]
 
 command = '/usr/bin/java SparseMatrixDriver '
 for i in sizes:
 	for j in range(5,100,5):
-		p = command + str(i) + " " + str(j)+ " >> " + str(i) + "_percent_matrixdriver_linked.txt"
+		p = command + str(i) + " " + str(j)+ " >> " + str(i) + "rpi_percent_matrixdriver_linked.txt"
 		subprocess.call(p, shell=True)
 		print(p)
