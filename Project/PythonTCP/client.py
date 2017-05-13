dc02d9a3ea072ff875ade137783bdf41442f1ac3
krohak <rohaksinghal14@gmail.com>
2017-05-13 21:08:17 +0000

    corrected lab8

--- Project/PythonTCP/client.py
+++ Project/PythonTCP/client.py
@@ -1,19 +1,31 @@
 import socket
 import sys
+import datetime
+from sense_hat import SenseHat
+
+sense = SenseHat()
 
 mysock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-try:	
-	ainfo=socket.getaddrinfo("192.168.1.2",1234)
-	mysock.connect(ainfo[0][4])
+try:
+        ainfo=socket.getaddrinfo("192.168.1.2",1234)
+        mysock.connect(ainfo[0][4])
+        date=str(datetime.datetime.now())
 
-	mysock.sendall("data")
-	
+        humidity = str(sense.get_humidity())
+        temp = str(sense.get_temperature())
+        pressure = str(sense.get_pressure())
+        north = str(sense.get_compass())
+
+        packet=str(date+','+temp+','+pressure+','+humidity+','+north).encode()
+        mysock.sendall(packet)
+
 
 except socket.error:
-	print("failed")
-	sys.exit(1)
+        print("failed")
+        sys.exit(1)
 
 
 
 mysock.close()
+
